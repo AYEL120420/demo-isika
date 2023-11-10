bc8fa9959590cb6e505a85dc98433bc8abeb9946
AYEL120224 <elaissaouiraounak0@gmail.com>
2023-11-10 12:07:05 +0000

    initial commit

---
+++ src/main/java/com/formation/service/Calculator.java
@@ -0,0 +1,19 @@
+package com.formation.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+	
+	public int sum(int a, int b) {
+		return a+b;
+	}
+	
+	public int multiply(int a, int b) {
+		return a*b;
+	}
+
+	public int divide(int a, int b) {
+		return a/b;
+	}
+}
